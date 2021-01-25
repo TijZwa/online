14f2ed3a2193a3695e83b824369b1c0ee4b318e2
Dennis Francis <dennis.francis@collabora.com>
2021-01-25 16:56:21 +0530

    introduce CanvasOverlay and its section
    TODO: Fix the Makefile.am duplication!
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I47d2f739b0d98c3926208358951e7240e632153f

--- loleaflet/src/layer/tile/CanvasSectionProps.js
+++ loleaflet/src/layer/tile/CanvasSectionProps.js
@@ -12,6 +12,7 @@ L.CSections.Debug = {}; // For keeping things simple.
 
 // First definitions. Other properties will be written according to their orders.
 L.CSections.Tiles = 				{ name: 'tiles'				, zIndex: 5 };
+L.CSections.Overlays =				{ name: 'overlay'			, zIndex: 5 };
 L.CSections.CalcGrid = 				{ name: 'calc grid'			, zIndex: 5 };
 L.CSections.Debug.Splits = 			{ name: 'splits'			, zIndex: 5 };
 L.CSections.Debug.TilePixelGrid = 	{ name: 'tile pixel grid'	, zIndex: 5 };
@@ -34,19 +35,21 @@ L.CSections.CornerHeader.processingOrder =			1; // Calc.
 L.CSections.RowHeader.processingOrder =				2; // Calc.
 L.CSections.ColumnHeader.processingOrder =			3; // Calc.
 L.CSections.Tiles.processingOrder = 				5; // Writer & Impress & Calc.
+L.CSections.Overlays.processingOrder =				5; // Writer & Impress & Calc.
 L.CSections.Debug.TilePixelGrid.processingOrder = 	5; // Writer & Impress & Calc. This is bound to tiles, processingOrder is not important.
 L.CSections.CalcGrid.processingOrder = 				5; // Calc. This is bound to tiles, processingOrder is not important.
 L.CSections.Debug.Splits.processingOrder = 			5; // Calc. This is bound to tiles, processingOrder is not important.
 
 
-L.CSections.CalcGrid.drawingOrder = 				4; // Calc. This is 7 when debugging is enabled.
+L.CSections.CalcGrid.drawingOrder = 				4; // Calc. This is 8 when debugging is enabled.
 L.CSections.Tiles.drawingOrder = 					5; // Writer & Impress & Calc.
 L.CSections.Debug.TilePixelGrid.drawingOrder = 		6; // Writer & Impress & Calc.
-/* drawingOrder = 7 is reserved for debugging mode of CalcGrid */
-L.CSections.Debug.Splits.drawingOrder = 			8; // Calc.
-L.CSections.RowHeader.drawingOrder = 				9; // Calc.
-L.CSections.ColumnHeader.drawingOrder = 			10; // Calc.
-L.CSections.CornerHeader.drawingOrder =				11; // Calc.
+L.CSections.Overlays.drawingOrder =					7; // Writer & Impress & Calc.
+/* drawingOrder = 8 is reserved for debugging mode of CalcGrid */
+L.CSections.Debug.Splits.drawingOrder = 			9; // Calc.
+L.CSections.RowHeader.drawingOrder = 				10; // Calc.
+L.CSections.ColumnHeader.drawingOrder = 			11; // Calc.
+L.CSections.CornerHeader.drawingOrder =				12; // Calc.
 
 
 
